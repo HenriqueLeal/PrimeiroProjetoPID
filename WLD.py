ac937c4a4715d76296a5ed73397833cb44dfa7ca
Henrique Leal <henrique.tavares@fatec.sp.gov.br>
2018-09-17 20:07:07 -0300

    Add files via upload

--- WLD.py
+++ WLD.py
@@ -8,7 +8,6 @@
 * WLD of Brodatz Album and Under the Curve Calc. *
 **************************************************
 '''
-
 descriptorgen = 1
 allimg = dir(mstring('*.gif'))
 imgn = length(allimg)
@@ -16,19 +15,26 @@ if descriptorgen:
     histogenerate = zeros(16, 96)
     histo = zeros(1, 96)
 	
-	
     for i in mslice[1:imgn]:
         found = allimg(i).name
         img = imread(found)
         [subimage1, subimage2, subimage3, subimage4, subimage5, subimage6, subimage7, subimage8, subimage9, subimage10, subimage11, subimage12, subimage13, subimage14, subimage15, subimage16] = cortaImagem(img)
-        histogenerate(1, mslice[:]).lvalue = calc(subimage1)
-        histogenerate(2, mslice[:]).lvalue = calc(subimage2)
-        histogenerate(3, mslice[:]).lvalue = calc(subimage3)
-        histogenerate(4, mslice[:]).lvalue = calc(subimage4)
-        histogenerate(5, mslice[:]).lvalue = calc(subimage5)
-        histogenerate(6, mslice[:]).lvalue = calc(subimage6)
-        histogenerate(7, mslice[:]).lvalue = calc(subimage7)
-        histogenerate(8, mslice[:]).lvalue = calc(subimage8)
+        histogenerate(1, 
+		              mslice[:]).lvalue = calc(subimage1)
+        histogenerate(2, 
+		              mslice[:]).lvalue = calc(subimage2)
+        histogenerate(3, 
+		              mslice[:]).lvalue = calc(subimage3)
+        histogenerate(4, 
+		              mslice[:]).lvalue = calc(subimage4)
+        histogenerate(5, 
+		              mslice[:]).lvalue = calc(subimage5)
+        histogenerate(6, 
+		              mslice[:]).lvalue = calc(subimage6)
+        histogenerate(7, 
+		              mslice[:]).lvalue = calc(subimage7)
+        histogenerate(8, 
+		              mslice[:]).lvalue = calc(subimage8)
         histogenerate(9, mslice[:]).lvalue = calc(subimage9)
         histogenerate(10, mslice[:]).lvalue = calc(subimage10)
         histogenerate(11, mslice[:]).lvalue = calc(subimage11)
@@ -38,25 +44,20 @@ if descriptorgen:
         histogenerate(15, mslice[:]).lvalue = calc(subimage15)
         histogenerate(16, mslice[:]).lvalue = calc(subimage16)
         histo = vertcat(histo, histogenerate)
-     
     histo(1, mslice[:]).lvalue = mcat([])
     for i in mslice[1:imgn]:
-        imag(mslice[(i * 16) - 15:i * 16], 1).lvalue = i
-     
+        imag(mslice[(i * 16) - 15:i * 16], 1).lvalue = i     
     frag = 0
     frag_template = mcat([1, OMPCSEMI, 2, OMPCSEMI, 3, OMPCSEMI, 4, OMPCSEMI, 5, OMPCSEMI, 6, OMPCSEMI, 7, OMPCSEMI, 8, OMPCSEMI, 9, OMPCSEMI, 10, OMPCSEMI, 11, OMPCSEMI, 12, OMPCSEMI, 13, OMPCSEMI, 14, OMPCSEMI, 15, OMPCSEMI, 16])
 	for i in mslice[1:imgn]:
     frag = vertcat(frag, frag_template)
- 
 frag(1, mslice[:]).lvalue = mcat([])
 template = horzcat(imag, frag)
 maxv = max(max(histo))
 minv = min(min(histo))
 for i in mslice[1:size(histo, 1)]:
     for j in mslice[1:size(histo, 2)]:
-        histo(i, j).lvalue = (histo(i, j) - minv) / maxv - minv
-     
- 
+        histo(i, j).lvalue = (histo(i, j) - minv) / maxv - minv 
 histo_identificavel = horzcat(template, histo)
 matriz_precision_recall = mcat([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 for i in mslice[1:imgn]:
@@ -83,9 +84,7 @@ def calc(img=None):
         for j in mslice[1:colunas]:
             a(i, j).lvalue = atan(vetor1(i, j) / vetor2(i, j))
             k = secondclassificator(a(i, j))
-            ext(k).lvalue = ext(k) + 1
-         
-     
+            ext(k).lvalue = ext(k) + 1     
 	v3 = filter2(filtro3, img)
     v4 = filter2(filtro4, img)
     ori = zeros(1, 12)
@@ -119,8 +118,6 @@ def firstclass(x=None):
         result = 11
     else:
         result = 12
-     
- 
 @mfunction("result")
 def secondclassificator(x=None):
     if logical_and((x >= (-0.5 * pi)), (x < (-0.3 * pi))):
@@ -139,8 +136,6 @@ def secondclassificator(x=None):
         result = 7
     else:
         result = 8
-     
- 
 @mfunction("precisionRecallCurve")
 def buscaClasseDatabase(classe=None, database=None):
     database_ord = ordenaPelaDistancia(classe, database)
@@ -154,21 +149,12 @@ def buscaClasseDatabase(classe=None, database=None):
             if (count == 1) and (divisor == 1):
             else:
                 precisionRecallCurve(1, count - 1).lvalue = count / divisor
-             
-         
-     
- 
 @mfunction("database")
 def ordenaPelaDistancia(classe=None, database=None):
     for i in mslice[1:size(database, 1)]:
         if database(i, 2) == 1:
             if database(i, 1) == classe:
-                valor = database(i, mslice[3:98])
-             
-         
-     
+                valor = database(i, mslice[3:98])     
     for i in mslice[1:size(database, 1)]:
-        database(i, 99).lvalue = norm(valor - database(i, mslice[3:98]))
-     
+        database(i, 99).lvalue = norm(valor - database(i, mslice[3:98]))    
     database = sortrows(database, 99)
- 
